f484ffbebe4d7ba3ef6fb7fcb3bd6c979bb775fc
Jorge <jorge.cywdt@gmail.com>
2018-11-27 17:09:22 -0500

    KKK

--- src/java/controlador/cadenasController.java
+++ src/java/controlador/cadenasController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controlador;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,22 +9,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import servicios.cadenas;
 
-/**
- *
- * @author Estudiante
- */
 @Controller
 public class cadenasController {
+    
     @Autowired
     cadenas cs;
+    
     @RequestMapping("/controller/vocales.htm")
-    protected ModelAndView muestraVocales(HttpServletRequest request, 
-                    HttpServletResponse response){
-        ModelAndView mv = new ModelAndView("/resvocales");
-        //String cad = "ejemplo";
-        String cad = request.getParameter("dato");
+    protected ModelAndView muestraVocales(HttpServletRequest request, HttpServletResponse response){
         
+        ModelAndView mv = new ModelAndView("/resvocales");
+        String cad = "ejemplo"; // String cad = request.getParameter("dato");                
         mv.addObject("resultado", cs.cuentaVocales(cad));
         return mv;
+        
     }
+    
 }
