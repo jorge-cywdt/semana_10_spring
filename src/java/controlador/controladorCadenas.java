5cf233251b7f6f1907d216bf6a00c2b2a21efdcd
Jorge <jorge.cywdt@gmail.com>
2018-11-27 17:37:20 -0500

    KKK

--- src/java/controlador/controladorCadenas.java
+++ src/java/controlador/controladorCadenas.java
@@ -1,26 +1,26 @@
 
 package controlador;
 
+import servicios.cadenas;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import servicios.cadenas;
 
 @Controller
-public class cadenasController {
+public class controladorCadenas {
     
     @Autowired
-    cadenas cs;
+    private cadenas objCad;
     
-    @RequestMapping("/controller/vocales.htm")
+    @RequestMapping("/controladorCadenas/vocales.htm")
     protected ModelAndView muestraVocales(HttpServletRequest request, HttpServletResponse response){
         
-        ModelAndView mv = new ModelAndView("/resvocales");
+        ModelAndView mv = new ModelAndView("/vocales");
         String cad = "ejemplo"; // String cad = request.getParameter("dato");                
-        mv.addObject("resultado", cs.cuentaVocales(cad));
+        mv.addObject("resultado", objCad.cuentaVocales(cad));
         return mv;
         
     }
