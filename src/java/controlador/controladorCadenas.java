f84a02e8d3c6db4f7bee1e4b9e51d80ee1bde063
Jorge <jorge.cywdt@gmail.com>
2018-11-27 17:42:34 -0500

    KKK

--- src/java/controlador/controladorCadenas.java
+++ src/java/controlador/controladorCadenas.java
@@ -1,13 +1,13 @@
 
 package controlador;
 
-import servicios.cadenas;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import servicios.cadenas;
 
 @Controller
 public class controladorCadenas {
