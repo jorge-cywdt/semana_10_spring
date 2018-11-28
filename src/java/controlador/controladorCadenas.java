986fd4147e44d721de7f37c9e2b974ce3f8c79d9
Jorge <jorge.cywdt@gmail.com>
2018-11-27 23:24:14 -0500

    KKK

--- src/java/controlador/controladorCadenas.java
+++ src/java/controlador/controladorCadenas.java
@@ -15,11 +15,11 @@ public class controladorCadenas {
     @Autowired
     private cadenas objCad;
     
-    @RequestMapping("/controladorCadenas/vocales.htm")
+    @RequestMapping("/vocales")
     public ModelAndView muestraVocales(HttpServletRequest request, HttpServletResponse response){
         
         ModelAndView mv = new ModelAndView("/vocales");
-        String cad = "ejemplo"; // String cad = request.getParameter("dato");                
+        String cad = "liliana"; // String cad = request.getParameter("dato");                
         mv.addObject("resultado", objCad.cuentaVocales(cad));
         return mv;
         
